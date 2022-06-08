b1d85c44f1400ef43737260e7f13a059fa77845a
ptw <ptw@PTWH-IT-LAPT-154.ptw-i.com>
2022-06-08 15:06:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const apikey = "5b144ab1309801d053996743eab223d1";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [info, setInfo] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fetchCurrent = async () => {
+      const current = `http://api.weatherstack.com/current?access_key=${apikey}&query=${city}`;
+      const response = await fetch(current);
+      const data = await response.json();
+
+      const cityInfo = {
+        Name: data.location.name,
+        Temperature: data.current.temperature,
+        Description: data.current.weather_descriptions[0],
+      };
+
+      setInfo(cityInfo);
+      setCity("");
+    };
+    fetchCurrent();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter City"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        ></input>
+        <button type="submit">Current</button>
+        <button type="submit">Historic</button>
+      </form>
+      <div>
+        <li>City: {info.Name}</li>
+        <li>Temperarure : {info.Temperature} °C</li>
+        <li>Description : {info.Description}</li>
+      </div>
     </div>
   );
 }
-
 export default App;
